714573524c799f91621267d7b5b5959beea3c326
unknown <biharrajya111@gmail.com>
2025-05-14 15:56:06 +0530

    'commit'

--- src/functionalProgramming/PrintList.java
+++ src/functionalProgramming/PrintList.java
@@ -6,14 +6,21 @@ public class PrintList {
 	
 	public static void printElementsFromList(List<Integer> numbers) {
 		
-		numbers.stream().forEach( x-> System.out.println(x));
+		//numbers.stream().forEach( x-> System.out.println(x));
+		numbers.stream().forEach(System.out::println);	
+	}
+	
+public static void printEvenElementsFromList(List<Integer> numbers) {
 		
+		//numbers.stream().forEach( x-> System.out.println(x));
+		numbers.stream().filter(x->x%2==0).forEach(System.out::println);	
 	}
 	
 	
 	public static void main(String[] args) {
 		
-		printElementsFromList(List.of(12,13,12,17,8,9));
+		//printElementsFromList(List.of(12,13,12,17,8,9));
+		printEvenElementsFromList(List.of(12,13,12,17,8,9));
 	}
 
 }
---
+++ src/functionalProgramming/Reduce.java
@@ -0,0 +1,33 @@
+package functionalProgramming;
+
+import java.util.List;
+
+public class Reduce {
+	
+	
+	public static int sum(int a, int b) {
+		
+		return a+b;
+	}
+
+
+	private static int addNumbersFromList(List<Integer> numbers) {
+		
+		//return numbers.stream().reduce(0, (x,y)-> x+y);
+		
+		//return numbers.stream().reduce(0, Reduce::sum);
+
+		return numbers.stream().reduce(0,Integer::sum);
+
+
+	}
+
+	public static void main(String[] args) {
+
+		int n=addNumbersFromList(List.of(12,13,45,66,98));
+		System.out.println(n);
+	}
+
+
+
+}
