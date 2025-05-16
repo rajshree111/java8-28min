6ee04239f2d590d0f1f02ce3482432a085a9ce95
unknown <biharrajya111@gmail.com>
2025-05-16 17:07:43 +0530

    unaryoperator function reference taken as variable

--- src/fibnocii/Java8.java
+++ src/fibnocii/Java8.java
@@ -1,5 +1,6 @@
 package fibnocii;
 
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 public class Java8 {
@@ -7,11 +8,13 @@ public class Java8 {
 	public static void main(String[] args) {
 		
 		System.out.println("Fibonacci Series up to 10 terms:");
+		
+		UnaryOperator<int[]> fibGen = fibRef ->new int[]{fibRef[1], fibRef[0] + fibRef[1]};
 
-        Stream.iterate(new int[]{0, 1}, fib -> new int[]{fib[1], fib[0] + fib[1]})
+        Stream.iterate(new int[]{0, 1}, fibGen)
               .limit(10)
               .map(fib -> fib[0])
-              .forEach(n -> System.out.print(n + " "));
+              .forEach(System.out::println);
     }
 
 }
